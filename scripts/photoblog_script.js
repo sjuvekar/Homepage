a730a339c43fc47deea69a7dee14e305d6e4c115
Sudeep Juvekar <sjuvekar@gmail.com>
2016-02-10 13:11:24 -0500

    photoblog
    Change-Id: Id90a8e805e600a552a0bbae206a967226e5314ed

---
+++ scripts/photoblog_script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	
+	$("#main").load("landscapes.html");
+	
+	$("#landscape").click( function() {
+		$("#main").load("landscapes.html")
+	});
+
+	$("#nature").click( function() {
+		$("#main").load("nature.html")
+	});
+
+	$("#people").click( function() {
+		$("#main").load("moments.html")
+	});
+});
